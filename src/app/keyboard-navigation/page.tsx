ee42c1e7c14ed84956386e85f2d2f55cd7246a6f
Guillermina Avigliano <guillerminaavigliano@yahoo.com.ar>
2023-05-26 15:25:12 -0300

    keyboard navigation focus debug

--- src/app/keyboard-navigation/page.tsx
+++ src/app/keyboard-navigation/page.tsx
@@ -1,10 +1,34 @@
+const focusedElementDebuggingCode = `
+document.body.addEventListener('focusin', (event) => {
+  console.log(document.activeElement)
+})`
 
+const focusedElementDebuggingComment = `
+// It logs the current focused element in the console.`
 
 export default function KeyboardNavigation() {
   return (
     <main>
-      <h1>Keyboard Navigation</h1>
+      <article>
+        <header>
+          <h1>Keyboard Navigation</h1>
+        </header>
 
+        <figure>
+          <figcaption>
+            Focused Element Debugging
+          </figcaption>
+          <pre>
+            <code className="comment">
+              {focusedElementDebuggingComment}
+            </code>
+            <code>
+              {focusedElementDebuggingCode}
+            </code>
+          </pre>
+        </figure>
+
+      </article>
     </main>
   )
 }
