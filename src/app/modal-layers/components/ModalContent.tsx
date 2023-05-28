cb5ec4b520d4bca1e1bd17517148a255bed27deb
Guillermina Avigliano <guillerminaavigliano@yahoo.com.ar>
2023-05-28 18:49:04 -0300

    fix close button autofocus

--- src/app/modal-layers/components/ModalContent.tsx
+++ src/app/modal-layers/components/ModalContent.tsx
@@ -1,10 +1,19 @@
+import { useEffect, useRef } from "react";
 import styles from "./ModalContent.module.css";
 
 const ModalContent = ({ onClose }: { onClose: () => void }) => {
+  const ref = useRef<HTMLButtonElement>(null);
+
+  useEffect(() => {
+    ref.current?.focus();
+  }, []);
+
   return (
     <div className={styles["modal"]}>
       <div>I am a modal dialog</div>
-      <button onClick={onClose}>Close</button>
+      <button onClick={onClose} ref={ref}>
+        Close
+      </button>
     </div>
   );
 };
